5e815aa8193d04dae7b3371695a3a8dd907a137d
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-01 12:20:43 +0600

    Update build.gradle.kts with Java 17 and Kotlin toolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -28,24 +29,30 @@ android {
             )
         }
     }
+    
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    
     buildFeatures {
         compose = true
     }
+    
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.2"
     }
+    
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+// এখানে kotlinOptions এর বদলে নতুন Toolchain ব্যবহার করা হলো
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
