2afe7dc91a6ba674db4b458feb6e653e0d2d7c64
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-01 12:39:35 +0600

    Update WebRTC to use JitPack dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,9 +68,9 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.4")
     
-    // WebRTC & Socket (ভার্সন ঠিক করা হয়েছে)
+    // WebRTC & Socket (JitPack থেকে নেওয়া হয়েছে)
     implementation("io.socket:socket.io-client:2.1.0")
-    implementation("org.webrtc:google-webrtc:1.0.30094")
+    implementation("com.github.nicememes:webrtc:113.0.0")
     
     // Debug
     debugImplementation("androidx.compose.ui:ui-tooling")
