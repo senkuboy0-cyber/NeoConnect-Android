a479720337cb2bc73b19a2404aa3d61c08004391
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-01 19:38:16 +0600

    Add Material Components dependency for theme support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.4")
-    // এই লাইনটি আইকন এরর ঠিক করবে
+    // Material Components (থিম এরর ঠিক করতে)
+    implementation("com.google.android.material:material:1.11.0")
+    // Icons
     implementation("androidx.compose.material:material-icons-extended")
     
     // WebRTC & Socket
